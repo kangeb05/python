11f523fd1dc49a9c59f98d9e69ace498ecd2e2cd
digh <a01025814180@gmail.com>
2025-03-17 11:54:29 +0900

    2주차 실습 코드

---
+++ 변수.py
@@ -0,0 +1,20 @@
+num1 = 100
+num2 = 200
+더하기 = num1 + num2
+print(num1,'+',num2,'=',더하기)
+
+빼기 = num1-num2
+print(num1,'-',num2,'=',빼기)
+
+곱하기 = num1*num2
+print(num1,'*',num2,'=',곱하기)
+
+나누기 = num1/num2
+print(num1,'/',num2,'=',나누기)
+
+str1 = 'hello'
+str2 = 'world'
+print(str1+' '+str2)
+
+
+
